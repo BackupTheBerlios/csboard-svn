10db2b931d77a5de3d62accc52b1c510eb12899a
uvsravikiran <uvsravikiran@c997b593-1e36-0410-8f0d-b61b45a3cd09>
2007-03-11 08:08:23 +0000

    Modified Files: 	src/ics/ObservableGamesWidget.cs

--- trunk/csboard/src/ics/ObservableGamesWidget.cs
+++ trunk/csboard/src/ics/ObservableGamesWidget.cs
@@ -43,14 +43,15 @@ namespace CsBoard
 			Entry filterEntry;
 
 			TreeModelFilter filter;
-			TreeIter ratedGamesIter;
-			TreeIter unratedGamesIter;
-			TreeIter examGamesIter;
+		  	TreeIter[,] iters; // [rated,unrated,examined, lightning,blitz,standard,others
+		  	const int LIGHTNING_CATEGORY_IDX = 2;
+		  	const int OTHER_CATEGORY_IDX = 3;
 
 			public ObservableGamesWidget (GameObservationManager
 						      observer)
 			{
 				obManager = observer;
+				iters = new TreeIter[3,4];
 				gamesView = new TreeView ();
 				infoLabel = new Label ();
 				infoLabel.Xalign = 0;
@@ -63,8 +64,6 @@ namespace CsBoard
 						       typeof (string),	// markup
 						       typeof (string),	// 
 						       typeof (string));
-
-				  AddParentIters ();
 
 				  gamesView.HeadersVisible = true;
 				  gamesView.HeadersClickable = true;
@@ -101,8 +100,16 @@ namespace CsBoard
 				  filter.VisibleFunc = FilterFunc;
 				  gamesView.Model = filter;
 
+				  AddParentIters ();
+
 				  infoLabel.UseMarkup = true;
 				  PackStart (infoLabel, false, true, 4);
+				  Label tipLabel = new Label();
+				  tipLabel.Xalign = 0;
+				  tipLabel.Xpad = 4;
+				  tipLabel.Markup = String.Format("<small><small><i>{0}</i></small></small>",
+								  Catalog.GetString("Press the refresh button to get an updated list of games.\nDouble click on a game to observe it."));
+				  PackStart(tipLabel, false, true, 4);
 				  PackStart (filterEntry, false, true, 4);
 				  PackStart (win, true, true, 4);
 
@@ -123,9 +130,27 @@ namespace CsBoard
 				  ShowAll ();
 			}
 
+			private void AddCategoryIters(int type, TreeIter parent) {
+				string[] names = new string[] {
+					Catalog.GetString("Blitz"),
+					Catalog.GetString("Standard"),
+					Catalog.GetString("Lightning"),
+					Catalog.GetString("Others")
+				};
+
+				for(int i = 0; i < names.Length; i++) {
+					iters[type,i] = store.AppendValues(parent, "", 0,
+									   String.Format("<b><i>{0}</i></b>", names[i]), "", "");
+					if(i < names.Length - 1) {
+						gamesView.ExpandRow(store.GetPath(iters[type,i]), false);
+					}
+				}
+			}
+
 			private void AddParentIters ()
 			{
-				ratedGamesIter = store.AppendValues ("",
+				TreeIter iter;
+				iter = store.AppendValues ("",
 								     0,
 								     String.
 								     Format
@@ -134,7 +159,10 @@ namespace CsBoard
 								      GetString
 								      ("Rated Games")),
 								     "", "");
-				unratedGamesIter =
+				AddCategoryIters(0, iter);
+				gamesView.ExpandRow(store.GetPath(iter), false);
+
+				iter =
 					store.AppendValues ("", 0,
 							    String.
 							    Format
@@ -143,7 +171,10 @@ namespace CsBoard
 							     GetString
 							     ("Unrated Games")),
 							    "", "");
-				examGamesIter =
+				AddCategoryIters(1, iter);
+				gamesView.ExpandRow(store.GetPath(iter), false);
+
+				iter =
 					store.AppendValues ("", 0,
 							    String.
 							    Format
@@ -152,6 +183,7 @@ namespace CsBoard
 							     GetString
 							     ("Examined Games")),
 							    "", "");
+				AddCategoryIters(2, iter);
 			}
 
 			private void OnFilter (object o, EventArgs args)
@@ -162,8 +194,14 @@ namespace CsBoard
 			protected bool FilterFunc (TreeModel model,
 						   TreeIter iter)
 			{
-				if (model.IterHasChild (iter))
+				TreePath path = model.GetPath (iter);
+				if(path.Depth <= 2) {
 					return true;
+				}
+				if(path.Indices[1] != OTHER_CATEGORY_IDX && path.Indices[1] != LIGHTNING_CATEGORY_IDX) {
+					path.Up();
+					gamesView.ExpandRow(path, false);
+				}
 
 				string filterstr = filterEntry.Text.Trim ();
 				if (filterstr.Length == 0)
@@ -216,20 +254,44 @@ namespace CsBoard
 				filter_string = filter_string.ToLower ();
 
 				UpdateInfoLabel ();
-				TreeIter iter =
-					details.
-					Examined ? examGamesIter : details.
-					Rated ? ratedGamesIter :
-					unratedGamesIter;
+				TreeIter iter;
+				bool needs_expansion;
+				GetIter(details, out iter, out needs_expansion);
 
-				store.AppendValues (iter,
-						    filter_string,
-						    details.gameId,
-						    details.
-						    ToPango (),
-						    details.
-						    TimeDetailsAsMarkup
-						    (), details.CategoryStr);
+				TreeIter child = store.PrependNode(iter);
+				int i = 0;
+				store.SetValue(child, i++, filter_string);
+				store.SetValue(child, i++, details.gameId);
+				store.SetValue(child, i++, details.ToPango ());
+				store.SetValue(child, i++, details.TimeDetailsAsMarkup());
+				store.SetValue(child, i++, details.CategoryStr);
+
+				if(needs_expansion)
+					gamesView.ExpandRow(store.GetPath(iter), false);
+			}
+
+			private void GetIter(GameDetails details, out TreeIter iter, out bool needs_expansion) {
+				int x = details.Examined ? 2 : details.Rated ? 1 : 2;
+				int y;
+				switch(details.GameCategory) {
+					case GameCategory.Blitz:
+						y = 0;
+						needs_expansion = true;
+						break;
+					case GameCategory.Standard:
+						y = 1;
+						needs_expansion = true;
+						break;
+					case GameCategory.Lightning:
+						y = 2;
+						needs_expansion = false;
+						break;
+					default:
+						y = 3;
+						needs_expansion = false;
+						break;
+				}
+				iter = iters[x,y];
 			}
 
 			public void Clear ()
